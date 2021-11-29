62ffb7021b625653979723a060f0cff954fc3137
Lee Siang Yin <siangyin916@gmail.com>
2021-11-29 17:58:39 +0800

    ⭕️ http object such as req.url

--- 02-express/app.js
+++ 02-express/app.js
@@ -1,9 +1,22 @@
 const http = require('http')
 
 const server = http.createServer((req, res) => {
- res.writeHead(200, { "content-type": "text/html" })
+console.log(req.url) // /about or /
+
+ if (req.url === "/") {
+   res.writeHead(200, { "content-type": "text/html" })
  res.write("<h1>welcome to homepage</h1>")
  res.end()
+ } else if(req.url === "/about") {
+   res.writeHead(200, { "content-type": "text/html" })
+ res.write("<h1>about page</h1>")
+ res.end()
+ } else {
+  res.writeHead(404, { "content-type": "text/html" })
+ res.write("<h1>page not found</h1>")
+ res.end()
+ }
+
 })
 
 server.listen(5000)
