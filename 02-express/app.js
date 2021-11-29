1563b5dab716295b00f41ad6598b47fd6edd67d1
Lee Siang Yin <siangyin916@gmail.com>
2021-11-29 17:50:02 +0800

    ⭕️ http header

--- 02-express/app.js
+++ 02-express/app.js
@@ -1,7 +1,9 @@
 const http = require('http')
 
 const server = http.createServer((req, res) => {
- console.log("user at server")
+ res.writeHead(200, { "content-type": "text/html" })
+ res.write("<h1>welcome to homepage</h1>")
+ res.end()
 })
 
 server.listen(5000)
