ee4ac6889e120d073dd22246052fc2a2dc06d318
Lee Siang Yin <siangyin916@gmail.com>
2021-11-30 16:33:53 +0800

    🔅 Basic Validation for schema

--- 03-task-manager/models/task.js
+++ 03-task-manager/models/task.js
@@ -2,7 +2,15 @@ const mongoose = require("mongoose")
 
 // only property pass in schema will be pass to MongoDB, whatever come extra will be ignored.
 const TaskSchema = new mongoose.Schema({
- name:String, completed:Boolean
+ name: {
+  type: String,
+  required: [true, "must provide name"],
+  trim: true,
+  maxlength:[20, "cannot more than 20 characters"]
+ }, completed: {
+  type: Boolean,
+  default: false
+ }
 })
 
 module.exports = mongoose.model('Task', TaskSchema)
