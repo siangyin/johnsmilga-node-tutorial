16e5fd83123d6372146a479ea39b8ed29855b641
Lee Siang Yin <siangyin916@gmail.com>
2021-11-30 16:27:15 +0800

    ℹ️ schema info

--- 03-task-manager/models/task.js
+++ 03-task-manager/models/task.js
@@ -1,6 +1,6 @@
 const mongoose = require("mongoose")
 
-
+// only property pass in schema will be pass to MongoDB, whatever come extra will be ignored.
 const TaskSchema = new mongoose.Schema({
  name:String, completed:Boolean
 })
