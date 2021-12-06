f4c8b221833531c46ad9a890b6afe2013a1c635d
Lee Siang Yin <siangyin916@gmail.com>
2021-12-06 18:49:18 +0800

    created static public folder to save front-end resources js, logo, styles

--- 04-expressbasic/app.js
+++ 04-expressbasic/app.js
@@ -2,11 +2,18 @@ const express = require("express");
 const app = express();
 const PORT = 3000;
 
+// to get the navbar-app access
+const path = require("path");
+
+// static public folder
+app.use(express.static("./public"));
+
 // standard methods
 
 app.get("/", (req, res) => {
-	res.status(200).send("homepage");
+	res.sendFile(path.resolve(__dirname, "./navbar-app/index.html"));
 });
+//path resolve to get the navbar-app html
 
 app.get("/about", (req, res) => {
 	res.status(200).send("about");
