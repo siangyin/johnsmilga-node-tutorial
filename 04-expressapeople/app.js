29738e04b9531c25ec96328c3573e6d56fa9df20
Lee Siang Yin <siangyin916@gmail.com>
2021-12-08 01:40:28 +0800

    deleting dup login route

--- 04-expressapeople/app.js
+++ 04-expressapeople/app.js
@@ -29,19 +29,6 @@ app.post("/login", (req, res) => {
 	// } else {res.status(401).json(`please input name`);}
 });
 
-// a path where the form used to submit data eg:	<form action="/login" method="POST">
-// in order for the data from form to be pass we need to use urlencoded middleware
-app.post("/login", (req, res) => {
-	// console.log(req.body); req.body will return [Object: null prototype] { name: 'yippee' }
-	const { name } = req.body;
-	res.status(200).json({ success: true, name: name });
-
-	// if name not falsy welcome else provide input
-	// if (name) {
-	// 	return res.status(200).json(`welcome ${name}`);
-	// } else {res.status(401).json(`please input name`);}
-});
-
 app.get("/api/people", (req, res) => {
 	res.status(200).json({ success: true, data: people });
 });
