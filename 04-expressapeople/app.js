6801defd8e0e3a0c213eb36e21102ce280b8532b
Lee Siang Yin <siangyin916@gmail.com>
2021-12-07 23:01:02 +0800

    ⚡️added static method to get public files access

--- 04-expressapeople/app.js
+++ 04-expressapeople/app.js
@@ -3,6 +3,14 @@ const app = express();
 const PORT = 3000;
 const { people } = require("./data");
 
+//static assets
+
+app.use(express.static("./methods-public"));
+
+app.get("/api/people", (req, res) => {
+	res.status(200).json({ success: true, data: people });
+});
+
 app.listen(PORT, (req, res) => {
 	console.log(`server listening on ${PORT}`);
 });
