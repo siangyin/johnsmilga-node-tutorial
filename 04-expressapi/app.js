d67403668bae89fd524239dc59fd850288ab3566
Lee Siang Yin <siangyin916@gmail.com>
2021-12-07 16:16:05 +0800

    testing url

--- 04-expressapi/app.js
+++ 04-expressapi/app.js
@@ -7,7 +7,7 @@ const { products } = require("./data");
 // middleware is function we can pass in the route, less repetition.
 const logger = (req, res, next) => {
 	const url = req.url;
-	console.log(`logger is here`);
+	console.log(`logger is here url ${url}`);
 	console.log(req.body);
 	// console.log(req.url, req.href, req.path);
 	next();
