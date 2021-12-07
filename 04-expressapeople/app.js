9743ef843cc4ced83e2107105badab1adb0a3e0b
Lee Siang Yin <siangyin916@gmail.com>
2021-12-08 00:07:10 +0800

    📝learned regular POST method to pass formdata with middleware: app.use(express.urlencoded({ extended: false }));

--- 04-expressapeople/app.js
+++ 04-expressapeople/app.js
@@ -3,12 +3,29 @@ const app = express();
 const PORT = 3000;
 const { people } = require("./data");
 
-//static assets
-
+//(middleware) static assets method to access public folder
 app.use(express.static("./methods-public"));
+
+//(middleware) parse form data
+// The express.urlencoded() function is a built-in middleware function in Express. It parses incoming requests with urlencoded payloads and is based on body-parser.
+// common approach using extended: false
+app.use(express.urlencoded({ extended: false }));
 
 app.get("/api/people", (req, res) => {
 	res.status(200).json({ success: true, data: people });
+});
+
+// a path where the form used to submit data eg:	<form action="/login" method="POST">
+// in order for the data from form to be pass we need to use urlencoded middleware
+app.post("/login", (req, res) => {
+	// console.log(req.body); req.body will return [Object: null prototype] { name: 'yippee' }
+	const { name } = req.body;
+	res.status(200).json({ success: true, name: name });
+
+	// if name not falsy welcome else provide input
+	// if (name) {
+	// 	return res.status(200).json(`welcome ${name}`);
+	// } else {res.status(401).json(`please input name`);}
 });
 
 app.listen(PORT, (req, res) => {
