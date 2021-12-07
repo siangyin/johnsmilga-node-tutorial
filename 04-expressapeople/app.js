c756728a62ae99e51fea726fc5b032f220fc0dc9
Lee Siang Yin <siangyin916@gmail.com>
2021-12-07 20:24:43 +0800

    💿 updated data

--- 04-expressapeople/app.js
+++ 04-expressapeople/app.js
@@ -1,6 +1,7 @@
 const express = require("express");
 const app = express();
 const PORT = 3000;
+const { people } = require("./data");
 
 app.listen(PORT, (req, res) => {
 	console.log(`server listening on ${PORT}`);
