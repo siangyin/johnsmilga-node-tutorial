7b165bb5d77ea97c2df290f5c62b34e963ff13a1
Lee Siang Yin <siangyin916@gmail.com>
2021-12-08 17:29:16 +0800

    completed all refactoring in controllers, and note must require the data in controller as well to make it work

--- 04-expressapeople/controllers/people.js
+++ 04-expressapeople/controllers/people.js
@@ -1,8 +1,60 @@
+let { people } = require("../data");
+
 const getPeople = (req, res) => {
 	res.status(200).json({ success: true, data: people });
 };
 
+const createPerson = (req, res) => {
+	const { name } = req.body;
+	if (!name) {
+		return res.status(400).json({ success: false, msg: "pls provide value" });
+	}
+	res.status(200).json({ success: true, person: name });
+};
 
+const deletePerson = (req, res) => {
+	const { id } = req.params;
 
+	//if no such id 404
+	const person = people.find((person) => person.id === Number(id));
+	if (!person) {
+		return res
+			.status(404)
+			.json({ success: false, msg: `no data with id${id}` });
+	}
 
-module.exports = { getPeople}
+	// if filter return is within one line dont need {} but if more than one line need {return ...}
+	const newData = people.filter((pax) => pax.id !== Number(id));
+	console.log(newData);
+	return res.status(200).json({ success: true, data: newData });
+};
+
+const updatePerson = (req, res) => {
+	const { id } = req.params;
+	const { name } = req.body;
+
+	const person = people.find((person) => person.id === Number(id));
+	if (!person) {
+		return res
+			.status(404)
+			.json({ success: false, msg: `no data with id${id}` });
+	}
+
+	// or alternative
+	// if (!people[id]) {
+	// 	return res
+	// 		.status(404)
+	// 		.json({ success: false, msg: "pls provide valid id" });
+	// }
+
+	// updating data
+	const newPpl = people.map((pax) => {
+		if (pax.id === Number(id)) {
+			pax.name = name;
+		}
+		return pax;
+	});
+	res.status(200).json({ success: true, data: newPpl });
+};
+
+module.exports = { getPeople, createPerson, deletePerson, updatePerson };
--- 04-expressapeople/routes/people.js
+++ 04-expressapeople/routes/people.js
@@ -2,64 +2,22 @@ const express = require("express");
 const router = express.Router();
 let { people } = require("../data");
 
-const { getPeople } = require("../controllers/people");
+const {
+	getPeople,
+	createPerson,
+	deletePerson,
+	updatePerson,
+} = require("../controllers/people");
 
 router.get("/", getPeople);
 
 // for javascript method
-router.post("/", (req, res) => {
-	const { name } = req.body;
-	if (!name) {
-		return res.status(400).json({ success: false, msg: "pls provide value" });
-	}
-	res.status(200).json({ success: true, person: name });
-});
+router.post("/", createPerson);
 
 // update data with put
-router.put("/:id", (req, res) => {
-	const { id } = req.params;
-	const { name } = req.body;
-
-	const person = people.find((person) => person.id === Number(id));
-	if (!person) {
-		return res
-			.status(404)
-			.json({ success: false, msg: `no data with id${id}` });
-	}
-
-	// or alternative
-	// if (!people[id]) {
-	// 	return res
-	// 		.status(404)
-	// 		.json({ success: false, msg: "pls provide valid id" });
-	// }
-
-	// updating data
-	const newPpl = people.map((pax) => {
-		if (pax.id === Number(id)) {
-			pax.name = name;
-		}
-		return pax;
-	});
-	res.status(200).json({ success: true, data: newPpl });
-});
+router.put("/:id", updatePerson);
 
 //deleting data route
-router.delete("/:id", (req, res) => {
-	const { id } = req.params;
-
-	//if no such id 404
-	const person = people.find((person) => person.id === Number(id));
-	if (!person) {
-		return res
-			.status(404)
-			.json({ success: false, msg: `no data with id${id}` });
-	}
-
-	// if filter return is within one line dont need {} but if more than one line need {return ...}
-	const newData = people.filter((pax) => pax.id !== Number(id));
-	console.log(newData);
-	return res.status(200).json({ success: true, data: newData });
-});
+router.delete("/:id", deletePerson);
 
 module.exports = router;
