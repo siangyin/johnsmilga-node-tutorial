a37aaa134a0b13fe5d101d6ce05825f738712c8d
Lee Siang Yin <siangyin916@gmail.com>
2021-12-10 13:43:58 +0800

    📝 learn limit && skill applying into pagination  📑
    eg:
     const page = Number(req.query.page) || 1;
      const limit = Number(req.query.limit) || 10;
      const skip = (page - 1) * limit;
    
      result = result.skip(skip).limit(limit);
      // 23 counts in 4pages if limit#7 (7+7+7+2)
    
      const products = await result;

--- 04-storeapi/controllers/products.js
+++ 04-storeapi/controllers/products.js
@@ -2,7 +2,16 @@ const Product = require("../models/products");
 
 const getAllProductsStatic = async (req, res) => {
 	// sorting result eg sort by name a-z, if -name is z-a if more than one sorting prop  space "name price" === name n price 0-9/a-z
-	const products = await Product.find({}).sort("name price");
+
+	//select, only showing the fields listed eg: {id:123,name:"kk",price:99}
+
+	//limit: to restricted returning result no.
+	//skip: to skip first few # result
+	const products = await Product.find({})
+		.sort("name price")
+		.select("name price")
+		.limit(10)
+		.skip(2);
 	res.status(200).json({ products, count: products.length });
 };
 
@@ -11,7 +20,7 @@ const getAllProducts = async (req, res) => {
 	// eg http://localhost:5000/api/products?featured=false&rating=5
 	// req.query return object so can directly pass in .find(obj)
 
-	const { featured, company, name, sort } = req.query;
+	const { featured, company, name, sort, fields } = req.query;
 	const queryObj = {};
 	if (featured) {
 		queryObj.featured = featured === "true" ? true : false;
@@ -27,12 +36,27 @@ const getAllProducts = async (req, res) => {
 
 	let result = Product.find(queryObj);
 
+	//______ sort ______//
 	if (sort) {
 		const sortList = sort.split(",").join(" ");
 		result = result.sort(sortList);
 	} else {
 		result = result.sort("createdAt");
 	}
+
+	//______ field ______//
+
+	if (fields) {
+		const selectList = fields.split(",").join(" ");
+		result = result.select(selectList);
+	}
+
+	//______ pagination ______//
+	const page = Number(req.query.page) || 1;
+	const limit = Number(req.query.limit) || 10;
+	const skip = (page - 1) * limit;
+
+	result = result.skip(skip).limit(limit);
 
 	const products = await result;
 	res.status(200).json({ products, count: products.length });
