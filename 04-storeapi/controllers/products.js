d5a85464090dd3dd95d8d2cc043238828b046d1b
Lee Siang Yin <siangyin916@gmail.com>
2021-12-10 12:51:18 +0800

    📝 learn to use custom query eg $regex ☀️
    ex 1: find name contain "a"
    const search = "a";
    	const products = await Product.find({
    		name: { $regex: search, $options: "i" },
    	});
    
    ex 2: using req.query path to determine queryObj to find
    const queryObj = {};
    
    if (featured) {
    		queryObj.featured = featured === "true" ? true : false;
    	}
    
    if (name) {
    		queryObj.name = { $regex: name, $options: "i" };
    	}
    
    const products = await Product.find(queryObj);

--- 04-storeapi/controllers/products.js
+++ 04-storeapi/controllers/products.js
@@ -1,7 +1,10 @@
 const Product = require("../models/products");
 
 const getAllProductsStatic = async (req, res) => {
-	const products = await Product.find({});
+	const search = "a";
+	const products = await Product.find({
+		name: { $regex: search, $options: "i" },
+	});
 	res.status(200).json({ products, count: products.length });
 };
 
@@ -9,7 +12,22 @@ const getAllProducts = async (req, res) => {
 	// for query path is ?key=value
 	// eg http://localhost:5000/api/products?featured=false&rating=5
 	// req.query return object so can directly pass in .find(obj)
-	const products = await Product.find(req.query);
+
+	const { featured, company, name } = req.query;
+	const queryObj = {};
+	if (featured) {
+		queryObj.featured = featured === "true" ? true : false;
+	}
+
+	if (company) {
+		queryObj.company = company;
+	}
+
+	if (name) {
+		queryObj.name = { $regex: name, $options: "i" };
+	}
+
+	const products = await Product.find(queryObj);
 	res.status(200).json({ products, count: products.length });
 };
 
