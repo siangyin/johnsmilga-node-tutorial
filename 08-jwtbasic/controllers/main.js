1ad6d04e7af1eaaad57fc22752791b398909c8af
Lee Siang Yin <siangyin916@gmail.com>
2021-12-24 13:31:56 +0800

    🚙 process with jwt token setup and tested fake login res.json token  🆗

--- 08-jwtbasic/controllers/main.js
+++ 08-jwtbasic/controllers/main.js
@@ -2,6 +2,8 @@
 // if exist create new JWT
 // send back to fron-end
 // setup authentication so only the request with JWT can access the dasboard
+
+const jwt = require("jsonwebtoken");
 const { CustomAPIError } = require("../errors/custom-err");
 
 const login = async (req, res) => {
@@ -16,7 +18,16 @@ const login = async (req, res) => {
 		throw new CustomAPIError("Please provide email and password", 400);
 	}
 
-	res.send("fake login/reg");
+	//just for demo, normally provided by DB!!!!
+	const id = new Date().getDate();
+
+	// try to keep payload small, better experience for user
+	// just for demo, in production use long, complex and unguessable string value!!!!!!!!!
+	const token = jwt.sign({ id, username }, process.env.JWT_SECRET, {
+		expiresIn: "30d",
+	});
+
+	res.status(200).json({ msg: "user created", token });
 };
 
 const dashboard = async (req, res) => {
