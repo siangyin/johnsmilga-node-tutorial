9bfc2df02016839ff574aeaedec329a397bf03fa
Lee Siang Yin <siangyin916@gmail.com>
2021-12-24 18:41:06 +0800

    ㊫ 🆕 Mongoose middleware :schema.pre("save",async fn)
    schema.pre('save', async function() {
      await doStuff();
      await doMoreStuff();
    });

--- 09-jobsapi/controllers/auth.js
+++ 09-jobsapi/controllers/auth.js
@@ -4,16 +4,17 @@ const { BadRequestError, UnauthenticatedError } = require("../errors");
 const bcrypt = require("bcryptjs");
 
 const register = async (req, res) => {
-	const { name, email, password } = req.body;
-	const salt = await bcrypt.genSalt(10);
-	const hashedPassword = await bcrypt.hash(password, salt);
-	const tempUser = { name, email, password: hashedPassword };
+	// UserSchema.pre replace all the following, bcs pw will be encrpt in UserSchema.pre instead
+	// const { name, email, password } = req.body;
+	// const salt = await bcrypt.genSalt(10);
+	// const hashedPassword = await bcrypt.hash(password, salt);
+	// const tempUser = { name, email, password: hashedPassword };
 
 	// if (!email || !password) {
 	// 	throw new BadRequestError("Please provide email and password");
 	// }
 
-	const user = await User.create({ ...tempUser });
+	const user = await User.create({ ...req.body });
 	res.status(StatusCodes.CREATED).json({
 		msg: `register user`,
 		data: user,
