f80dfe24dcf082d53d859ac3d656da8442d27d5b
Lee Siang Yin <siangyin916@gmail.com>
2021-12-24 14:34:21 +0800

    🔐 check auth & verify token, test in FE 🆗 change customErr to manual res.status().json then it worked ✅

--- 08-jwtbasic/controllers/main.js
+++ 08-jwtbasic/controllers/main.js
@@ -4,7 +4,7 @@
 // setup authentication so only the request with JWT can access the dasboard
 
 const jwt = require("jsonwebtoken");
-const { CustomAPIError } = require("../errors/custom-err");
+// const { CustomAPIError } = require("../errors/custom-err");
 
 const login = async (req, res) => {
 	const { username, password } = req.body;
@@ -15,7 +15,8 @@ const login = async (req, res) => {
 	// check in the controller
 
 	if (!username || !password) {
-		throw new CustomAPIError("Please provide email and password", 400);
+		res.status(400).json({ msg: "Please provide email and password" });
+		// throw new CustomAPIError("Please provide email and password", 400);
 	}
 
 	//just for demo, normally provided by DB!!!!
@@ -32,16 +33,29 @@ const login = async (req, res) => {
 
 const dashboard = async (req, res) => {
 	const authHeader = req.headers.authorization;
-	const token = authHeader.split(" ")[1];
 
 	// check if headers Authorization exist or does is start with bearer
 	if (!authHeader || !authHeader.startsWith("Bearer")) {
-		throw new CustomAPIError("no token provided", 401);
+		res.status(401).json({ msg: "no token provided" });
+		// throw new CustomAPIError("no token provided", 401);
 	}
-	const luckyNum = Math.floor(Math.random() * 100);
-	res
-		.status(200)
-		.send({ msg: `hello john`, secret: `here is ur auth data ${token}` });
+
+	const token = authHeader.split(" ")[1];
+
+	try {
+		const decoded = jwt.verify(token, process.env.JWT_SECRET);
+
+		const luckyNum = Math.floor(Math.random() * 100);
+		res
+			.status(200)
+			.send({
+				msg: `hello ${decoded.username}`,
+				secret: `here is ur auth data ${token}`,
+			});
+	} catch (err) {
+		res.status(401).json({ msg: "no authorised to acces this route" });
+		// throw new CustomAPIError("no authorised to acces this route", 401);
+	}
 };
 
 module.exports = { login, dashboard };
