d2464a29de25a1433ebe68eef8d167e7c73f0ae0
Lee Siang Yin <siangyin916@gmail.com>
2021-12-24 21:14:59 +0800

    👩🏻‍🔧 troubleshooting/testing ref user not working

--- 09-jobsapi/controllers/jobs.js
+++ 09-jobsapi/controllers/jobs.js
@@ -13,7 +13,8 @@ const getJob = async (req, res) => {
 const createJob = async (req, res) => {
 	// req.body.createdBy = req.user.userId;
 	const job = await Job.create(req.body);
-	res.status(StatusCodes.CREATED).json({ req });
+	console.log(req.body);
+	res.status(StatusCodes.CREATED).json({ job });
 };
 
 const updateJob = async (req, res) => {
