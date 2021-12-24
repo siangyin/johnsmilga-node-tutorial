882633115a6f8bf656863923997ea20a2835368e
Lee Siang Yin <siangyin916@gmail.com>
2021-12-24 18:52:21 +0800

    🔐 generated token, post tested 🆗

--- 09-jobsapi/controllers/auth.js
+++ 09-jobsapi/controllers/auth.js
@@ -2,6 +2,7 @@ const User = require("../models/User");
 const { StatusCodes } = require("http-status-codes");
 const { BadRequestError, UnauthenticatedError } = require("../errors");
 const bcrypt = require("bcryptjs");
+const jwt = require("jsonwebtoken");
 
 const register = async (req, res) => {
 	// UserSchema.pre replace all the following, bcs pw will be encrpt in UserSchema.pre instead
@@ -15,9 +16,16 @@ const register = async (req, res) => {
 	// }
 
 	const user = await User.create({ ...req.body });
+
+	// jwt
+	const token = jwt.sign({ userID: user._id, name: user.name }, "jwtSecret", {
+		expiresIn: "30d",
+	});
+
 	res.status(StatusCodes.CREATED).json({
 		msg: `register user`,
 		data: user,
+		token: token,
 	});
 };
 
