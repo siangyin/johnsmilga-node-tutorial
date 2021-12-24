fe466ccadba7736243a54ba377d12afbce3c9d16
Lee Siang Yin <siangyin916@gmail.com>
2021-12-24 19:25:45 +0800

    🔐 login controller 🆗

--- 09-jobsapi/controllers/auth.js
+++ 09-jobsapi/controllers/auth.js
@@ -25,7 +25,25 @@ const register = async (req, res) => {
 };
 
 const login = async (req, res) => {
-	res.status(200).json({ msg: `login user` });
+	const { email, password } = req.body;
+
+	if (!email || !password) {
+		throw new BadRequestError("Please provide email and password");
+	}
+
+	const user = await User.findOne({ email });
+	// comparing password
+	if (!user) {
+		throw new UnauthenticatedError("Invalid credentials");
+	}
+
+	// if user exist
+	const token = user.createJWT();
+	res.status(StatusCodes.OK).json({
+		msg: `User ${user.name} logged-in`,
+		data: user,
+		token: token,
+	});
 };
 
 module.exports = {
