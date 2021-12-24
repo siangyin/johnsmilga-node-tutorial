cc837348c3885b4ad1ff4e80dc11c236c41aa4bf
Lee Siang Yin <siangyin916@gmail.com>
2021-12-24 22:41:09 +0800

    ✅ duplicate value error

--- 09-jobsapi/controllers/jobs.js
+++ 09-jobsapi/controllers/jobs.js
@@ -53,7 +53,19 @@ const updateJob = async (req, res) => {
 };
 
 const deleteJob = async (req, res) => {
-	res.status(200).json({ msg: `deleteJob` });
+	const {
+		user: { userId },
+		params: { id: jobId },
+	} = req;
+
+	const job = await Job.findByIdAndRemove({
+		_id: jobId,
+		createdBy: userId,
+	});
+	if (!job) {
+		throw new NotFoundError(`No job with id ${jobId}`);
+	}
+	res.status(StatusCodes.OK).send(`Job with id ${jobId} deleted`);
 };
 module.exports = {
 	getAllJobs,
