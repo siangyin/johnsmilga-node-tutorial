c203aaf48effd03d73216ade6647a9158638e176
Lee Siang Yin <siangyin916@gmail.com>
2021-12-25 00:19:48 +0800

    ⚙️ heroku deploy prep and swagger doc generating
    note: deploy heroku
    "start": "nodemon app.js"
    "engines": {
    		"node": "14.x"
    	}
    Procfile: web: node app.js

--- 09-jobsapi/app.js
+++ 09-jobsapi/app.js
@@ -29,13 +29,26 @@ app.use(
 		max: 100, // limit each IP to 100 requests per windowMs
 	})
 );
+
+// Swagger
+const swaggerUI = require("swagger-ui-express");
+const YAML = require("yamljs");
+const swaggerDocument = YAML.load("./swagger.yaml");
+
 app.use(express.json());
 // extra packages
 app.use(helmet());
 app.use(cors());
 app.use(xss());
 
+app.get("/", (req, res) => {
+	res.send('<h1>Jobs API</h1><a href="/api-docs">Documentation</a>');
+});
+
+app.use("/api-docs", swaggerUI.serve, swaggerUI.setup(swaggerDocument));
+
 // routes
+
 app.use("/api/v1/auth", authRouter);
 app.use("/api/v1/jobs", authenticateUser, jobsRouter);
 
