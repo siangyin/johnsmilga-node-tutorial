d76b1fb736fe7d12e58b7b3c78ed838dd3dcc8f9
Lee Siang Yin <siangyin916@gmail.com>
2021-12-24 22:05:28 +0800

    🆗 get all jobs and get job

--- 09-jobsapi/controllers/jobs.js
+++ 09-jobsapi/controllers/jobs.js
@@ -3,11 +3,25 @@ const { StatusCodes } = require("http-status-codes");
 const { BadRequestError, NotFoundError } = require("../errors");
 
 const getAllJobs = async (req, res) => {
-	res.status(200).json({ msg: `getAllJobs` });
+	const jobs = await Job.find({ createdBy: req.user.userId }).sort("createdAt");
+	res.status(StatusCodes.OK).json({ jobs, count: jobs.length });
+	
 };
 
 const getJob = async (req, res) => {
-	res.status(200).json({ msg: `getJob` });
+	const {
+		user: { userId },
+		params: { id: jobId },
+	} = req;
+
+	const job = await Job.findOne({
+		_id: jobId,
+		createdBy: userId,
+	});
+	if (!job) {
+		throw new NotFoundError(`No job with id ${jobId}`);
+	}
+	res.status(StatusCodes.OK).json({ job });
 };
 
 const createJob = async (req, res) => {
