3eceae3dad09ba90825ea028cf161257b2f52d3f
Lee Siang Yin <siangyin916@gmail.com>
2022-01-03 17:25:55 +0800

    💳 stripeController

--- 12-stripe-payment/controllers/stripeController.js
+++ 12-stripe-payment/controllers/stripeController.js
@@ -1,1 +1,20 @@
+const stripe = require("stripe")(process.env.STRIPE_KEY);
 
+const stripeController = async (req, res) => {
+	console.log(req.body);
+	const { purchase, total_amount, shipping_fee } = req.body;
+
+	const calculateOrderAmount = () => {
+		return total_amount + shipping_fee;
+	};
+
+	const paymentIntent = await stripe.paymentIntents.create({
+		amount: calculateOrderAmount(),
+		currency: "usd",
+	});
+	console.log(paymentIntent);
+	// res.send("stripe route");
+	res.json({ clientSecret: paymentIntent.client_secret });
+};
+
+module.exports = stripeController;
--- 12-stripe-payment/public/browser-app.js
+++ 12-stripe-payment/public/browser-app.js
@@ -6,7 +6,7 @@ const total_amount = 10998;
 const shipping_fee = 1099;
 
 var stripe = Stripe(
-  'pk_test_51I87djFp5pnuKUXgBVIHiR36vVAWyfuyb7ckrhgyDNA1kM0GWHas9ZGUAgwJSFNUxrbyE6NwlMNmls1iGSfzHDdE00DQB3y6AH'
+	"pk_test_51KDm0JKWYMXNhyMsRGKUdfunDwScTLdKU6W0IALIzsVMLPcPnJ01Z3m9zQWM30L42QRkdHyGH8kco2P22MHbCZX3002tAdHVHm"
 );
 
 // The items the customer wants to buy
