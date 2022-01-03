60b1447bf1d3c28bc678e493f6656c731dc52423
Lee Siang Yin <siangyin916@gmail.com>
2022-01-03 15:43:32 +0800

    🏗 setup starter files

---
+++ 11-send-email/controllers/sendEmail.js
@@ -0,0 +1,12 @@
+const nodemailer = require("nodemailer");
+const sgMail = require("@sendgrid/mail");
+
+const sendEmailEthereal = async (req, res) => {
+	res.send("send email");
+};
+
+const sendEmail = async (req, res) => {
+	res.send("send email");
+};
+
+module.exports = sendEmail;
