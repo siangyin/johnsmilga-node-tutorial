4e231421fbfc814b03412af4407fb71f651848e4
Lee Siang Yin <siangyin916@gmail.com>
2022-01-03 14:06:44 +0800

    check if file exist, correct format, within size limit

--- 10-file-upload/controllers/uploadsController.js
+++ 10-file-upload/controllers/uploadsController.js
@@ -1,9 +1,23 @@
 const path = require("path");
-
 const { StatusCodes } = require("http-status-codes");
+const CustomError = require("../errors");
 
 const uploadProductImage = async (req, res) => {
+	// check if file exists
+	if (!req.files) {
+		throw new CustomError.BadRequestError("no file uploaded");
+	}
+
 	let productImage = req.files.image;
+	// check format
+	if (!productImage.mimetype.startsWith("image")) {
+	throw new CustomError.BadRequestError("please upload image");
+}
+	// check size
+	const maxSize = 1024 * 1024;
+	if (productImage.size > maxSize) {
+		throw new CustomError.BadRequestError("Please upload image smaller 1MB");
+	}
 	const imgPath = path.join(
 		__dirname,
 		"../public/uploads/" + `${productImage.name}`
