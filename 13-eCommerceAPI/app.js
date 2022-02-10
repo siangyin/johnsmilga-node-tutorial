abf3d4e1eeb386c2b88dfa4d5f65116b7002fa20
Lee Siang Yin <siangyin916@gmail.com>
2022-02-10 16:03:01 +0800

    ✅ basic route, middleware: notfound, errorhandler, morgan, and user model

--- 13-eCommerceAPI/app.js
+++ 13-eCommerceAPI/app.js
@@ -1,16 +1,27 @@
 require("dotenv").config();
+require("express-async-errors");
 const express = require("express");
 const app = express();
 const PORT = process.env.PORT || 5000;
 const connectDB = require("./db/connect");
+const morgan = require("morgan");
+
+//middleware
+app.use(morgan("tiny"));
+app.use(express.json());
+const notFoundMiddleware = require("./middleware/not-found");
+const errorHandlerMiddleware = require("./middleware/error-handler");
 
 app.get("/", (req, res) => {
 	res.send("Hello");
 });
 
+app.use(notFoundMiddleware);
+app.use(errorHandlerMiddleware);
+
 const start = async () => {
-  try {
-    await connectDB(process.env.MONGO_URL);
+	try {
+		await connectDB(process.env.MONGO_URL);
 		app.listen(PORT, console.log(`server is on port ${PORT}`));
 	} catch (error) {}
 };
