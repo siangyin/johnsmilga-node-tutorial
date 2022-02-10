31b583e749d7d0cac6b7d1a01af0339a4b3a769c
Lee Siang Yin <siangyin916@gmail.com>
2022-02-10 13:03:15 +0800

    🏗 basic express server and connect to mongoDB

--- 13-eCommerceAPI/app.js
+++ 13-eCommerceAPI/app.js
@@ -1,1 +1,18 @@
-console.log('E-Commerce API');
+require("dotenv").config();
+const express = require("express");
+const app = express();
+const PORT = process.env.PORT || 5000;
+const connectDB = require("./db/connect");
+
+app.get("/", (req, res) => {
+	res.send("Hello");
+});
+
+const start = async () => {
+  try {
+    await connectDB(process.env.MONGO_URL);
+		app.listen(PORT, console.log(`server is on port ${PORT}`));
+	} catch (error) {}
+};
+
+start();
