9fce73dbbf66f57aa8b059a351351e2e2bbaa8ae
Lee Siang Yin <siangyin916@gmail.com>
2022-02-11 22:53:09 +0800

    ✅ logout

--- 13-eCommerceAPI/controllers/authController.js
+++ 13-eCommerceAPI/controllers/authController.js
@@ -48,7 +48,12 @@ const login = async (req, res) => {
 };
 
 const logout = async (req, res) => {
-	res.send("logout user");
+	// const fivesecond = 1000 * 5;
+	res.cookie("token", "logout", {
+		httpOnly: true,
+		expires: new Date(Date.now()),
+	});
+	res.status(StatusCodes.OK).json({ msg: "user logged out!" });
 };
 
 module.exports = {
