c906193f7de48424a8f6c7066eac8b776aa028d4
Lee Siang Yin <siangyin916@gmail.com>
2022-02-11 23:24:35 +0800

    ✅ get all users & single users

--- 13-eCommerceAPI/controllers/userController.js
+++ 13-eCommerceAPI/controllers/userController.js
@@ -1,13 +1,20 @@
 const User = require("../models/User");
-const { StatusCodes, CREATED } = require("http-status-codes");
+const { StatusCodes } = require("http-status-codes");
 const CustomError = require("../errors");
 
 const getAllUsers = async (req, res) => {
-	res.send("get all users");
+	console.log(req.user);
+	const users = await User.find({ role: "user" }).select("-password");
+	res.status(StatusCodes.OK).json({ users });
 };
 
 const getSingleUser = async (req, res) => {
-	res.send("get Single user");
+	const user = await User.findOne({ _id: req.params.id }).select("-password");
+	if (!user) {
+		throw new CustomError.NotFoundError(`No user with id : ${req.params.id}`);
+	}
+	checkPermissions(req.user, user._id);
+	res.status(StatusCodes.OK).json({ user });
 };
 
 const showCurrentUser = async (req, res) => {
