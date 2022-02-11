f884607af1dbc0d264f443005090ca0f82916a9e
Lee Siang Yin <siangyin916@gmail.com>
2022-02-11 22:29:16 +0800

    🔑 Login route

--- 13-eCommerceAPI/controllers/authController.js
+++ 13-eCommerceAPI/controllers/authController.js
@@ -18,14 +18,33 @@ const register = async (req, res) => {
 
 	const user = await User.create({ name, email, password, role });
 
-	const tokenUser = { name, role, userID: user._id };
+	const tokenUser = { userID: user._id, name: user.name, role: user.role };
 
 	attachCookiesToResponse({ res, user: tokenUser });
 	res.status(StatusCodes.CREATED).json({ user: tokenUser });
 };
 
 const login = async (req, res) => {
-	res.send("login user");
+	const { email, password } = req.body;
+	if (!email || !password) {
+		throw new CustomError.BadRequestError("Please provide email and password");
+	}
+
+	const user = await User.findOne({ email });
+
+	if (!user) {
+		throw new CustomError.UnauthenticatedError("Invalid Credentials");
+	}
+
+	const isPasswordCorrect = await user.comparePassword(password);
+	if (!isPasswordCorrect) {
+		throw new CustomError.UnauthenticatedError("Invalid Credentials");
+	}
+
+	const tokenUser = { userID: user._id, name: user.name, role: user.role };
+
+	attachCookiesToResponse({ res, user: tokenUser });
+	res.status(StatusCodes.CREATED).json({ user: tokenUser });
 };
 
 const logout = async (req, res) => {
