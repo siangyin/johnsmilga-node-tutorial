e4cf011c0e74e2d0b172f6ca9d7adecd612574f2
Lee Siang Yin <siangyin916@gmail.com>
2022-02-13 22:11:48 +0800

    ✅ Reviews > get single delete, and update review

--- 13-eCommerceAPI/controllers/reviewController.js
+++ 13-eCommerceAPI/controllers/reviewController.js
@@ -41,14 +41,49 @@ const getAllReviews = async (req, res) => {
 };
 
 const getSingleReview = async (req, res) => {
-	res.send("getSingleReview");
+	const { id: reviewId } = req.params;
+
+	const review = await Review.findOne({ _id: reviewId });
+
+	if (!review) {
+		throw new CustomError.NotFoundError(`No review with id ${reviewId}`);
+	}
+
+	res.status(StatusCodes.OK).json({ review });
 };
+
 const updateReview = async (req, res) => {
-	res.send("updateReview");
+	const { id: reviewId } = req.params;
+	const { rating, title, comment } = req.body;
+
+	const review = await Review.findOne({ _id: reviewId });
+
+	if (!review) {
+		throw new CustomError.NotFoundError(`No review with id ${reviewId}`);
+	}
+
+	checkPermissions(req.user, review.user);
+
+	review.rating = rating;
+	review.title = title;
+	review.comment = comment;
+
+	await review.save();
+	res.status(StatusCodes.OK).json({ review });
 };
 
 const deleteReview = async (req, res) => {
-	res.send("deleteReview");
+	const { id: reviewId } = req.params;
+
+	const review = await Review.findOne({ _id: reviewId });
+
+	if (!review) {
+		throw new CustomError.NotFoundError(`No review with id ${reviewId}`);
+	}
+
+	checkPermissions(req.user, review.user);
+	await review.remove();
+	res.status(StatusCodes.OK).json({ msg: "Success! Review removed" });
 };
 
 module.exports = {
