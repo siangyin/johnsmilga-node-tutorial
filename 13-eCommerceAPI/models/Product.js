90c61d1116a2362ca1a33f02b516d453556760ba
Lee Siang Yin <siangyin916@gmail.com>
2022-02-13 22:39:49 +0800

    💡 Schema.pre("remove"... to remove all related item in other schema
    eg delete product, before remove will to remove this product's review in review schema tehn remove product in product schema.

--- 13-eCommerceAPI/models/Product.js
+++ 13-eCommerceAPI/models/Product.js
@@ -77,8 +77,8 @@ ProductSchema.virtual("reviews", {
 	justOne: false,
 });
 
-// ProductSchema.pre("remove", async function (next) {
-// 	await this.model("Review").deleteMany({ product: this._id });
-// });
+ProductSchema.pre("remove", async function (next) {
+	await this.model("Review").deleteMany({ product: this._id });
+});
 
 module.exports = mongoose.model("Product", ProductSchema);
