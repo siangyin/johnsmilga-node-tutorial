4b08e217a09e581649eac17575ed014cef5c5ac3
Lee Siang Yin <siangyin916@gmail.com>
2022-02-13 21:57:01 +0800

    ✅ Review > create review 🆗

--- 13-eCommerceAPI/controllers/reviewController.js
+++ 13-eCommerceAPI/controllers/reviewController.js
@@ -5,14 +5,41 @@ const { StatusCodes } = require("http-status-codes");
 const CustomError = require("../errors");
 const { checkPermissions } = require("../utils");
 
-// createReview, getAllReviews, getSingleReview, updateReview, deleteReview,
-
 const createReview = async (req, res) => {
-	res.send("createReview");
+	const { product: productId } = req.body;
+
+	const isValidProduct = await Product.findOne({ _id: productId });
+
+	if (!isValidProduct) {
+		throw new CustomError.NotFoundError(`No product with id : ${productId}`);
+	}
+
+	const alreadySubmitted = await Review.findOne({
+		product: productId,
+		user: req.user.userID,
+	});
+
+	if (alreadySubmitted) {
+		throw new CustomError.BadRequestError(
+			"Already submitted review for this product"
+		);
+	}
+
+	req.body.user = req.user.userID;
+
+	const review = await Review.create(req.body);
+	res.status(StatusCodes.CREATED).json({ review });
 };
+
 const getAllReviews = async (req, res) => {
-	res.send("getAllReviews");
+	const reviews = await Review.find({}).populate({
+		path: "product",
+		select: "name company price",
+	});
+
+	res.status(StatusCodes.OK).json({ reviews, count: reviews.length });
 };
+
 const getSingleReview = async (req, res) => {
 	res.send("getSingleReview");
 };
