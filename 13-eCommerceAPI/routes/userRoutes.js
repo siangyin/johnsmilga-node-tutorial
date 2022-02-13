4c99cba1dadfd757ad888d985d8404e16789ef6e
Lee Siang Yin <siangyin916@gmail.com>
2022-02-13 16:57:09 +0800

    ✅ update user password

--- 13-eCommerceAPI/routes/userRoutes.js
+++ 13-eCommerceAPI/routes/userRoutes.js
@@ -15,7 +15,7 @@ const {
 router.route("/").get(authenticateUser, authorizePermissions, getAllUsers);
 router.route("/showMe").get(authenticateUser, showCurrentUser);
 router.route("/updateUser").patch(updateUser);
-router.route("/updateUserPassword").patch(updateUserPassword);
+router.route("/updateUserPassword").patch(authenticateUser, updateUserPassword);
 router.route("/:id").get(authenticateUser, getSingleUser);
 
 module.exports = router;
