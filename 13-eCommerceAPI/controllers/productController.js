f7f9307388200f95a26a230b95b89f8f44e888e3
Lee Siang Yin <siangyin916@gmail.com>
2022-02-13 20:20:45 +0800

    ⚙️ Product schema, routes and controller setup

---
+++ 13-eCommerceAPI/controllers/productController.js
@@ -0,0 +1,37 @@
+const Product = require("../models/Product");
+const { StatusCodes } = require("http-status-codes");
+const CustomError = require("../errors");
+// const path = require("path");
+
+const createProduct = async (req, res) => {
+	res.send("createProduct");
+};
+
+const getAllProducts = async (req, res) => {
+	res.send("getAllProducts");
+};
+
+const getSingleProduct = async (req, res) => {
+	res.send("getSingleProduct");
+};
+
+const updateProduct = async (req, res) => {
+	res.send("updateProduct");
+};
+
+const deleteProduct = async (req, res) => {
+	res.send("deleteProduct");
+};
+
+const uploadImage = async (req, res) => {
+	res.send("uploadImage");
+};
+
+module.exports = {
+	createProduct,
+	getAllProducts,
+	getSingleProduct,
+	updateProduct,
+	deleteProduct,
+	uploadImage,
+};
