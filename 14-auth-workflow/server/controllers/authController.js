1c7d8ad62b2807837da4696b59c00805e165d7be
Lee Siang Yin <siangyin916@gmail.com>
2022-02-14 17:26:29 +0800

    ✅ AuthController: verifiedEmailController & route added

--- 14-auth-workflow/server/controllers/authController.js
+++ 14-auth-workflow/server/controllers/authController.js
@@ -34,6 +34,29 @@ const register = async (req, res) => {
 		verificationToken,
 	});
 };
+
+const verifyEmail = async (req, res) => {
+	const { verificationToken, email } = req.body;
+	const user = await User.findOne({ email });
+
+	if (!user) {
+		throw new CustomError.UnauthenticatedError("Verification Failed");
+	}
+
+	if (user.verificationToken !== verificationToken) {
+		throw new CustomError.UnauthenticatedError("Verification Failed");
+	}
+
+	// (user.isVerified = true), (user.verified = Date.now());
+	// user.verificationToken = "";
+
+	// await user.save();
+
+	res
+		.status(StatusCodes.OK)
+		.json({ msg: "Email Verified", verificationToken, email });
+};
+
 const login = async (req, res) => {
 	const { email, password } = req.body;
 
@@ -71,4 +94,5 @@ module.exports = {
 	register,
 	login,
 	logout,
+	verifyEmail,
 };
