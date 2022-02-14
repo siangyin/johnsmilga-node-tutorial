e576e9be55773299e4fbc1d451f7ed581f60964f
Lee Siang Yin <siangyin916@gmail.com>
2022-02-14 18:43:02 +0800

    ✅ updated email add

--- 14-auth-workflow/server/utils/sendEmail.js
+++ 14-auth-workflow/server/utils/sendEmail.js
@@ -14,8 +14,8 @@ const sendEmail = async () => {
 
 	// send mail with defined transport object
 	let info = await transporter.sendMail({
-		from: '"Fred Foo 👻" <foo@example.com>', // sender address
-		to: "bar@example.com, baz@example.com", // list of receivers
+		from: '"Super Sender 👻" <siangyin916@gmail.com>', // sender address
+		to: "yippee_oooo@hotmail.com", // list of receivers
 		subject: "Hello ✔", // Subject line
 		text: "Hello world?", // plain text body
 		html: "<b>Hello world?</b>", // html body
